44b4dd1fecb355fb8032d1277194876184831668
steve <steve.nguyen2013@gmail.com>
2022-04-24 17:17:43 -1000

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-function addingEventListener() {
+/*function addingEventListener() {
+    const input = document.getElementById('input');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
 }
+*/
+function addingEventListener(){
+    const input = document.getElementById('input');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert);
+}
+addingEventListener(); //this is to call the outer function, tested and alert was "I was clicked"
+//both versions work, as stated, as long as the event listener is inside the function addingEventListener. 
